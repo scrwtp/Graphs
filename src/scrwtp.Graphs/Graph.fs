c33ee004dde9ece7c4dae926c14d74211ff58d40
scrwtp <scrwtp@gmail.com>
2014-01-12 22:07:38 +0100

    * edited some comments

--- src/scrwtp.Graphs/Graph.fs
+++ src/scrwtp.Graphs/Graph.fs
@@ -3,21 +3,21 @@
 open System
 
 module Graphs = 
-    // take an edge and switch the start and end vertices
+    // Take an edge and switch the start and end vertices.
     let invert = function ((v1, v2), weight) -> ((v2, v1), weight)
 
-    // recognizer for deconstructing graphs in a transparent manner
+    // Recognizer for deconstructing graphs in a transparent manner.
     let (|Graph|_|)  graph = 
         match graph with
         | Directed (v, e)   -> Some (v, e)
         | Undirected (v, e) -> Some (v, Set.union e (Set.map invert e))
 
-    // get the graph edges; uses the recognizer so for each undirected edge will get a pair of directed edges
+    // Get the graph edges; uses the recognizer so for each undirected edge will get a pair of directed edges.
     let getEdges = function 
         | Graph (v, e) -> e
         | other -> failwithf "Not a graph: %A" other
 
-    // builds a weighted adjacency list from a set of edges
+    // Builds a weighted adjacency list from a set of edges.
     let buildAdjacencyList =
         getEdges
         >> Seq.map (fun ((v1, v2), weight) -> (v1, (v2, weight)))
@@ -27,13 +27,13 @@ module Graphs =
         >> Map.ofSeq
         >> AdjacencyList.Weighted
 
-    // builds an adjacency matrix from a set of edges
+    // Builds an adjacency matrix from a set of edges.
     let buildAdjacencyMatrix = 
         getEdges 
         >> Map.ofSeq
         >> AdjacencyMatrix.Sparse
 
-    // gets the neighbour vertices using an adjacency list
+    // Gets the neighbour vertices using an adjacency list.
     let neighbours adjacencyList vertex = 
         match adjacencyList with
         | Simple list -> 
